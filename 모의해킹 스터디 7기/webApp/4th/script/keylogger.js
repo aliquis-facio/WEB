6548614f3dbd25cffc47f3866c558d929bc2e947
aliquis-facio <jeonych305@gmail.com>
2024-11-10 16:31:23 +0900

    update 4th week

--- 모의해킹 스터디 7기/webApp/4th/script/keylogger.js
+++ 모의해킹 스터디 7기/webApp/4th/script/keylogger.js
@@ -1,9 +1,9 @@
 function keylogger_id() {
     let id = document.getElementById('id').value;
-    document.getElementById("result_id").innerText = id;
+    document.getElementById("result_id").innerText = "id: " + id;
 }
 
 function keylogger_pw() {
     let pw = document.getElementById('pw').value;
-    document.getElementById("result_pw").innerText = pw;
+    document.getElementById("result_pw").innerText = "pw: " + pw;
 }
---
+++ 모의해킹 스터디 7기/webApp/4th/script/test.js
@@ -0,0 +1,2 @@
+let elem = document.getElementById("test");
+elem.innerHTML = "10" + 1;
